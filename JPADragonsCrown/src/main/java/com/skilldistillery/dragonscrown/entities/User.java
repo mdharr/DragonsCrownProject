d9b91bf97d302b42ab7d556aeafcc7bc28e83414
Michael Harrington <mdharr7@gmail.com>
2024-02-13 14:04:25 +0800

    Test User entity

--- JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/User.java
+++ JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/User.java
@@ -3,11 +3,17 @@ package com.skilldistillery.dragonscrown.entities;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private String username;
 	private String password;
 	private Boolean enabled;
