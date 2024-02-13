4ee055491c1ee7e207fb1b8706e769d5b8783e3d
Michael Harrington <mdharr7@gmail.com>
2024-02-13 12:06:04 +0800

    Stub JPA and Boot/REST projects

---
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/DragonsCrownApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dragonscrown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonsCrownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonsCrownApplication.class, args);
+	}
+
+}
---
+++ DragonsCrown/src/test/java/com/skilldistillery/dragonscrown/DragonsCrownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dragonscrown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonsCrownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
