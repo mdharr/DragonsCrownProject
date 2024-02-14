4bc31976da910269e91f97eeda626f1767395c28
Michael Harrington <mdharr7@gmail.com>
2024-02-14 22:31:39 +0800

    Write two methods in player class service for find all and find by id

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassService.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassService.java
@@ -1,8 +1,11 @@
 package com.skilldistillery.dragonscrown.services;
 
+import java.util.List;
+
 import com.skilldistillery.dragonscrown.entities.PlayerClass;
 
 public interface PlayerClassService {
 
-	public PlayerClass getById(int playerClassId);
+	public List<PlayerClass> findAll();
+	public PlayerClass find(int playerClassId);
 }
--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassServiceImpl.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassServiceImpl.java
@@ -1,15 +1,32 @@
 package com.skilldistillery.dragonscrown.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.dragonscrown.entities.PlayerClass;
+import com.skilldistillery.dragonscrown.repositories.PlayerClassRepository;
 
 @Service
 public class PlayerClassServiceImpl implements PlayerClassService {
 
+	@Autowired
+	private PlayerClassRepository playerClassRepo;
+	
+	
 	@Override
-	public PlayerClass getById(int playerClassId) {
-		// TODO Auto-generated method stub
+	public List<PlayerClass> findAll() {
+		return playerClassRepo.findAll();
+	}
+	
+	@Override
+	public PlayerClass find(int playerClassId) {
+		PlayerClass playerClass = playerClassRepo.findById(playerClassId);
+		if(playerClass != null) {
+			return playerClass;
+		}
 		return null;
 	}
 
