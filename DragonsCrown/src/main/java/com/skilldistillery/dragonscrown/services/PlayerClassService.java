ddad7e15e654c3b0d9ed844ccbcbb731f879a126
Michael Harrington <mdharr7@gmail.com>
2024-02-14 22:22:55 +0800

    Create service and implementation for player class

---
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassService.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.dragonscrown.services;
+
+import com.skilldistillery.dragonscrown.entities.PlayerClass;
+
+public interface PlayerClassService {
+
+	public PlayerClass getById(int playerClassId);
+}
---
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassServiceImpl.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.dragonscrown.services;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.dragonscrown.entities.PlayerClass;
+
+@Service
+public class PlayerClassServiceImpl implements PlayerClassService {
+
+	@Override
+	public PlayerClass getById(int playerClassId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
