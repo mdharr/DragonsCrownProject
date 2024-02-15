69229cff46df2495af460190e52b0914c6a36f95
Michael Harrington <mdharr7@gmail.com>
2024-02-15 13:37:18 +0800

    Stub out index all method in player class service for retrieving all player class data from REST API endpoint

--- ngDragonsCrown/src/app/services/player-class.service.ts
+++ ngDragonsCrown/src/app/services/player-class.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { PlayerClass } from '../models/player-class';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerClassService {
+  private url = environment.baseUrl + 'api/classes';
 
-  constructor() { }
+  http = inject(HttpClient);
+  authService = inject(AuthService);
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  indexAll(): Observable<PlayerClass[]> {
+    return this.http.get<PlayerClass[]>(`${this.url}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('PlayerClassService.indexAll(): error retrieving list of player classes' + err)
+        );
+      })
+    );
+  }
 }
