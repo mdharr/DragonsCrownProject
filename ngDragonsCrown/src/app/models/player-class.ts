386dc6f85eb054c6620f23ed8bc1d1370c99f528
Michael Harrington <mdharr7@gmail.com>
2024-02-15 13:28:56 +0800

    Complete player class model in angular

--- ngDragonsCrown/src/app/models/player-class.ts
+++ ngDragonsCrown/src/app/models/player-class.ts
@@ -1,2 +1,28 @@
 export class PlayerClass {
+
+  id: number;
+  name: string;
+  description: string;
+  animationUrl: string;
+  artworkUrl: string;
+  titleUrl: string;
+  // classStats: ClassStats[];
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    description: string = '',
+    animationUrl: string = '',
+    artworkUrl: string = '',
+    titleUrl: string = '',
+    // classStats: ClassStats[] = []
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.animationUrl = animationUrl;
+    this.artworkUrl = artworkUrl;
+    this.titleUrl = titleUrl;
+    // this.classStats = classStats;
+  }
 }
