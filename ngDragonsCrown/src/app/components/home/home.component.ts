9b6d344228dbe0b6f5c05bd846bbcc7dbe7c181a
Michael Harrington <mdharr7@gmail.com>
2024-02-15 14:37:18 +0800

    Implement sprite animation for sorceress class on data load

--- ngDragonsCrown/src/app/components/home/home.component.ts
+++ ngDragonsCrown/src/app/components/home/home.component.ts
@@ -290,6 +290,13 @@ export class HomeComponent implements OnInit, OnDestroy {
     if(luck !== null) {
       luck.textContent = sorceressData.classStats[0].luck.toString();
     }
+    if(gifWrapper) {
+      const styles = gifWrapper.style;
+      styles.display = 'flex';
+    }
+    if(gifElement) {
+      gifElement.setAttribute('src', sorceressData.animationUrl);
+    }
   }
 
   loadWizardData() {
