6cb2fd94c61aec77538ee775af9ba529579a434a
Michael Harrington <mdharr7@gmail.com>
2024-02-15 10:14:08 +0800

    Write class stats service methods to be later implemented.

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/ClassStatsService.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/ClassStatsService.java
@@ -1,5 +1,11 @@
 package com.skilldistillery.dragonscrown.services;
 
+import java.util.List;
+
+import com.skilldistillery.dragonscrown.entities.ClassStats;
+
 public interface ClassStatsService {
 
+	public List<ClassStats> findAll();
+	public ClassStats findById(int classStatsId);
 }
