b04f6fe2d68ff055890fc48602197db7fc357d66
Michael Harrington <mdharr7@gmail.com>
2024-02-15 10:16:11 +0800

    Implement and override class stats methods in service implementation

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/ClassStatsServiceImpl.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/ClassStatsServiceImpl.java
@@ -1,8 +1,11 @@
 package com.skilldistillery.dragonscrown.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.dragonscrown.entities.ClassStats;
 import com.skilldistillery.dragonscrown.repositories.ClassStatsRepository;
 
 @Service
@@ -10,4 +13,18 @@ public class ClassStatsServiceImpl implements ClassStatsService {
 
 	@Autowired
 	private ClassStatsRepository classStatsRepo;
+
+	@Override
+	public List<ClassStats> findAll() {
+		return classStatsRepo.findAll();
+	}
+
+	@Override
+	public ClassStats findById(int classStatsId) {
+		ClassStats classStats = classStatsRepo.findById(classStatsId);
+		if(classStats != null) {
+			return classStats;
+		}
+		return null;
+	}
 }
