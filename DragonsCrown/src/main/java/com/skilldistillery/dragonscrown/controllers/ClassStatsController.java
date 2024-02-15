66b053462b9bb59a868a2897a713e9e24061f96f
Michael Harrington <mdharr7@gmail.com>
2024-02-15 10:25:14 +0800

    Write REST API for getting all classes stats

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/controllers/ClassStatsController.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/controllers/ClassStatsController.java
@@ -1,5 +1,34 @@
 package com.skilldistillery.dragonscrown.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.dragonscrown.entities.ClassStats;
+import com.skilldistillery.dragonscrown.services.ClassStatsService;
+
+@RestController
+@CrossOrigin({"*", "http://localhost"})
+@RequestMapping("api")
 public class ClassStatsController {
 
+	@Autowired
+	private ClassStatsService classStatsService;
+	
+	@GetMapping("classStats")
+	public ResponseEntity<List<ClassStats>> getAllClassStats() {
+		List<ClassStats> classStats = classStatsService.findAll();
+		if (classStats.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		else {
+			return new ResponseEntity<>(classStats, HttpStatus.OK);
+		}
+	}
 }
