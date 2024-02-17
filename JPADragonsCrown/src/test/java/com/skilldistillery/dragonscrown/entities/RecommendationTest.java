e63fb6b1672f62d44d49e4bd717a6289e51da9c6
Michael Harrington <mdharr7@gmail.com>
2024-02-17 22:21:27 +0800

    Stub out JUnit test case for recommendation entity. Test success.

--- JPADragonsCrown/src/test/java/com/skilldistillery/dragonscrown/entities/RecommendationTest.java
+++ JPADragonsCrown/src/test/java/com/skilldistillery/dragonscrown/entities/RecommendationTest.java
@@ -2,6 +2,10 @@ package com.skilldistillery.dragonscrown.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,26 +13,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RecommendationTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Recommendation recommendation;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPADragonsCrown");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		recommendation = em.find(Recommendation.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		recommendation = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_Recommendation_entity_mapping() {
+		assertNotNull(recommendation);
+		assertEquals("I want to play as an orthodox warrior who wields a sword.", recommendation.getDescription());
 	}
 
 }
