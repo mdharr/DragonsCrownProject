2697df2d8f56aef70fe8c3fc9e6e08d31dedcea1
Michael Harrington <mdharr7@gmail.com>
2024-02-17 22:26:10 +0800

    Generate service implementation for recommendation entity.

---
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/RecommendationServiceImpl.java
@@ -0,0 +1,23 @@
+package com.skilldistillery.dragonscrown.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.dragonscrown.entities.Recommendation;
+
+@Service
+public class RecommendationServiceImpl implements RecommendationService {
+
+	@Override
+	public List<Recommendation> findAll() {
+		return null;
+	}
+
+	@Override
+	public Recommendation find(int recommendationId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
