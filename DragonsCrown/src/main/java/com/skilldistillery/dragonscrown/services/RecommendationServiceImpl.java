8a85de88a83c3b15383b1d3bcdf0d605829a4f50
Michael Harrington <mdharr7@gmail.com>
2024-02-17 22:27:44 +0800

    Stub out service implementation for recommendation entity.

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/RecommendationServiceImpl.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/RecommendationServiceImpl.java
@@ -2,21 +2,29 @@ package com.skilldistillery.dragonscrown.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.dragonscrown.entities.Recommendation;
+import com.skilldistillery.dragonscrown.repositories.RecommendationRepository;
 
 @Service
 public class RecommendationServiceImpl implements RecommendationService {
 
+	@Autowired
+	private RecommendationRepository recommendationRepo;
+	
 	@Override
 	public List<Recommendation> findAll() {
-		return null;
+		return recommendationRepo.findAll();
 	}
 
 	@Override
 	public Recommendation find(int recommendationId) {
-		// TODO Auto-generated method stub
+		Recommendation recommendation = recommendationRepo.findById(recommendationId);
+		if(recommendation != null) {
+			return recommendation;
+		}
 		return null;
 	}
 
