88123a02c3a08e37c0e8a4846f3754f55a0951eb
Michael Harrington <mdharr7@gmail.com>
2024-02-18 12:34:38 +0800

    Generate controller for stat scaling entity.

---
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/controllers/StatScalingController.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.dragonscrown.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin({"*", "http://localhost"})
+@RequestMapping("api")
+public class StatScalingController {
+
+}
