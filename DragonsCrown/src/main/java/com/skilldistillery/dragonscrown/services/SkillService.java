1290549b57a4da46c1c3ace61560d018563c3082
Michael Harrington <mdharr7@gmail.com>
2024-02-20 22:23:40 +0800

    Add methods to skill service

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/SkillService.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/SkillService.java
@@ -1,5 +1,11 @@
 package com.skilldistillery.dragonscrown.services;
 
+import java.util.List;
+
+import com.skilldistillery.dragonscrown.entities.Skill;
+
 public interface SkillService {
 
+	public List<Skill> findAll();
+	public Skill findById(int skillId);
 }
--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/SkillServiceImpl.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/SkillServiceImpl.java
@@ -1,8 +1,24 @@
 package com.skilldistillery.dragonscrown.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.dragonscrown.entities.Skill;
 
 @Service
 public class SkillServiceImpl implements SkillService {
+
+	@Override
+	public List<Skill> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Skill findById(int skillId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
