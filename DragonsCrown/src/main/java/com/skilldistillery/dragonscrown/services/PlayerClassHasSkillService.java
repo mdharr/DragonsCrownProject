ba7a79aadadd64e8df7e24cd8640d721bf8a7eae
Michael Harrington <mdharr7@gmail.com>
2024-02-20 22:22:56 +0800

    Add methods to player has skill service

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassHasSkillService.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassHasSkillService.java
@@ -1,5 +1,11 @@
 package com.skilldistillery.dragonscrown.services;
 
+import java.util.List;
+
+import com.skilldistillery.dragonscrown.entities.PlayerClassHasSkill;
+
 public interface PlayerClassHasSkillService {
 
+	public List<PlayerClassHasSkill> findAll();
+	public PlayerClassHasSkill findById(int playerClassHasSkillId);
 }
--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassHasSkillServiceImpl.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassHasSkillServiceImpl.java
@@ -1,8 +1,24 @@
 package com.skilldistillery.dragonscrown.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.dragonscrown.entities.PlayerClassHasSkill;
 
 @Service
 public class PlayerClassHasSkillServiceImpl implements PlayerClassHasSkillService {
+
+	@Override
+	public List<PlayerClassHasSkill> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PlayerClassHasSkill findById(int playerClassHasSkillId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
