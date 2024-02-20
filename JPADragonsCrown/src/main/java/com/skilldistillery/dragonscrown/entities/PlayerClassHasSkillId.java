60e34a01c74e4a8228316b513029f7f50d64b5a0
Michael Harrington <mdharr7@gmail.com>
2024-02-20 21:06:58 +0800

    Stub out constructors, getters/setters for player class has skill id embeddable

--- JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/PlayerClassHasSkillId.java
+++ JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/PlayerClassHasSkillId.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.dragonscrown.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -17,4 +18,58 @@ public class PlayerClassHasSkillId implements Serializable {
 	
 	@Column(name = "skill_id")
 	private int skillId;
+
+	public PlayerClassHasSkillId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlayerClassHasSkillId(int playerClassId, int skillId) {
+		super();
+		PlayerClassId = playerClassId;
+		this.skillId = skillId;
+	}
+
+	public int getPlayerClassId() {
+		return PlayerClassId;
+	}
+
+	public void setPlayerClassId(int playerClassId) {
+		PlayerClassId = playerClassId;
+	}
+
+	public int getSkillId() {
+		return skillId;
+	}
+
+	public void setSkillId(int skillId) {
+		this.skillId = skillId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(PlayerClassId, skillId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PlayerClassHasSkillId other = (PlayerClassHasSkillId) obj;
+		return PlayerClassId == other.PlayerClassId && skillId == other.skillId;
+	}
+
+	@Override
+	public String toString() {
+		return "PlayerClassHasSkillId [PlayerClassId=" + PlayerClassId + ", skillId=" + skillId + "]";
+	}
+	
 }
