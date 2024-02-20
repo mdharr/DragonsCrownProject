2f4acc5731e76bf96f8ecea4ec22c6664ac16113
Michael Harrington <mdharr7@gmail.com>
2024-02-20 21:54:52 +0800

    Stub out repository for skill details

---
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/repositories/SkillDetails.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.dragonscrown.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillDetails extends JpaRepository<SkillDetails, Integer> {
+
+}
