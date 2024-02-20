38b2f45f0a3d8e115d909dd69d3a120d66a356af
Michael Harrington <mdharr7@gmail.com>
2024-02-20 21:10:29 +0800

    Add fields to skill entity

--- JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/Skill.java
+++ JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/Skill.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.dragonscrown.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,4 +12,13 @@ public class Skill {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	private String name;
+	private String description;
+	@Column(name = "card_image_url")
+	private String cardImageUrl;
+	@Column(name = "is_common")
+	private boolean isCommon;
+	
+	
 }
