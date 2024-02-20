1fc9dc34987d866a2c93e6748a7cfe339c974d4b
Michael Harrington <mdharr7@gmail.com>
2024-02-20 22:24:10 +0800

    Add methods to skill details service

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/SkillDetailsService.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/SkillDetailsService.java
@@ -1,5 +1,11 @@
 package com.skilldistillery.dragonscrown.services;
 
+import java.util.List;
+
+import com.skilldistillery.dragonscrown.entities.SkillDetails;
+
 public interface SkillDetailsService {
 
+	public List<SkillDetails> findAll();
+	public SkillDetails findById(int skillDetailsId);
 }
