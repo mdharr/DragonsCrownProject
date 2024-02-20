85d0fd62036f66d246e83a3b918411cec74ba84b
Michael Harrington <mdharr7@gmail.com>
2024-02-20 22:26:46 +0800

    Stub out find all and find by id methods in skill details service impl

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/SkillDetailsServiceImpl.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/SkillDetailsServiceImpl.java
@@ -1,8 +1,31 @@
 package com.skilldistillery.dragonscrown.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.dragonscrown.entities.SkillDetails;
+import com.skilldistillery.dragonscrown.repositories.SkillDetailsRepository;
 
 @Service
 public class SkillDetailsServiceImpl implements SkillDetailsService {
+	
+	@Autowired
+	private SkillDetailsRepository skillDetailsRepo;
+
+	@Override
+	public List<SkillDetails> findAll() {
+		return skillDetailsRepo.findAll();
+	}
+
+	@Override
+	public SkillDetails findById(int skillDetailsId) {
+		SkillDetails skillDetails = skillDetailsRepo.findById(skillDetailsId);
+		if (skillDetails != null) {
+			return skillDetails;
+		}
+		return null;
+	}
 
 }
