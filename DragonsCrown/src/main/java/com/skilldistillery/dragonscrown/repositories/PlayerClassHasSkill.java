dc3ad1fe2e16cac5645c500e446433805f460a59
Michael Harrington <mdharr7@gmail.com>
2024-02-20 21:56:01 +0800

    Add find by id method to player class has skill repository

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/repositories/PlayerClassHasSkill.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/repositories/PlayerClassHasSkill.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerClassHasSkill extends JpaRepository<PlayerClassHasSkill, Integer> {
 
+	PlayerClassHasSkill findById(int playerClassHasSkillId);
 }
