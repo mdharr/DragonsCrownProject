e348588f1aef77b1c7cfbb76ea7a92309ef392ee
Michael Harrington <mdharr7@gmail.com>
2024-02-20 20:57:59 +0800

    Add entity annotation along with embedded id to player class has skill

--- JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/PlayerClassHasSkill.java
+++ JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/PlayerClassHasSkill.java
@@ -1,5 +1,13 @@
 package com.skilldistillery.dragonscrown.entities;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "player_class_has_skill")
 public class PlayerClassHasSkill {
 
+	@EmbeddedId
+	private PlayerClassHasSkillId id;
 }
