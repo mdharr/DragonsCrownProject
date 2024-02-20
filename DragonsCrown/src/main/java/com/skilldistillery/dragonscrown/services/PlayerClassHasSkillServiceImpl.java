4f9df726429ab9fbc564e80ef24b54be5a1005a5
Michael Harrington <mdharr7@gmail.com>
2024-02-20 22:29:50 +0800

    Stub out find all and find by id methods in player class has skill service impl

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassHasSkillServiceImpl.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassHasSkillServiceImpl.java
@@ -2,22 +2,29 @@ package com.skilldistillery.dragonscrown.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.dragonscrown.entities.PlayerClassHasSkill;
+import com.skilldistillery.dragonscrown.repositories.PlayerClassHasSkillRepository;
 
 @Service
 public class PlayerClassHasSkillServiceImpl implements PlayerClassHasSkillService {
+	
+	@Autowired
+	private PlayerClassHasSkillRepository playerClassHasSkillRepo;
 
 	@Override
 	public List<PlayerClassHasSkill> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return playerClassHasSkillRepo.findAll();
 	}
 
 	@Override
 	public PlayerClassHasSkill findById(int playerClassHasSkillId) {
-		// TODO Auto-generated method stub
+		PlayerClassHasSkill playerClassHasSkill = playerClassHasSkillRepo.findById(playerClassHasSkillId);
+		if (playerClassHasSkill != null) {
+			return playerClassHasSkill;
+		}
 		return null;
 	}
 
