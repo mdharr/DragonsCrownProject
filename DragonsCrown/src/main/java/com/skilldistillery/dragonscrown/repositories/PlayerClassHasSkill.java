1e7e83254502b0f5be426d03cdde698ebfbb3174
Michael Harrington <mdharr7@gmail.com>
2024-02-20 21:53:44 +0800

    Stub out repository for player class has skill

---
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/repositories/PlayerClassHasSkill.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.dragonscrown.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerClassHasSkill extends JpaRepository<PlayerClassHasSkill, Integer> {
+
+}
