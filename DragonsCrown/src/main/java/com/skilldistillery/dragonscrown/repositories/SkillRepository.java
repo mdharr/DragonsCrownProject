73d940b050923a6f922620f92b69cf1989db2517
Michael Harrington <mdharr7@gmail.com>
2024-02-20 22:36:35 +0800

    Add new method for is common property true in skill repo

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/repositories/SkillRepository.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/repositories/SkillRepository.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.dragonscrown.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.dragonscrown.entities.Skill;
@@ -7,4 +9,5 @@ import com.skilldistillery.dragonscrown.entities.Skill;
 public interface SkillRepository extends JpaRepository<Skill, Integer> {
 
 	Skill findById(int skillId);
+	List<Skill> findByIsCommonTrue();
 }
