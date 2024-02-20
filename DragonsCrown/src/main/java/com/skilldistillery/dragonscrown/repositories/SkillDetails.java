fd0f4d3968c59cefa419a7eafde46e7fdecc824a
Michael Harrington <mdharr7@gmail.com>
2024-02-20 21:56:39 +0800

    Add find by id method to skill details repository

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/repositories/SkillDetails.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/repositories/SkillDetails.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SkillDetails extends JpaRepository<SkillDetails, Integer> {
 
+	SkillDetails findById(int skillDetailsId);
 }
