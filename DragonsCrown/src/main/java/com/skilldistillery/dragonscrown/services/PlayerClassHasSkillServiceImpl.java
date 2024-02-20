27ece575645795418c7b5ef762c9a35cff7b7d27
Michael Harrington <mdharr7@gmail.com>
2024-02-20 22:17:44 +0800

    Generate service interface and service implementation class for player class has skill

---
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/services/PlayerClassHasSkillServiceImpl.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.dragonscrown.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayerClassHasSkillServiceImpl implements PlayerClassHasSkillService {
+
+}
