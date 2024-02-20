558bef9a52077d695f4684b84f46114c100e5c12
Michael Harrington <mdharr7@gmail.com>
2024-02-20 21:03:42 +0800

    Add embeddable annotation and implement serializable in player class has skill id class

--- JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/PlayerClassHasSkillId.java
+++ JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/PlayerClassHasSkillId.java
@@ -1,5 +1,14 @@
 package com.skilldistillery.dragonscrown.entities;
 
-public class PlayerClassHasSkillId {
+import java.io.Serializable;
 
+import javax.persistence.Embeddable;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "player_class_has_skill")
+public class PlayerClassHasSkillId implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
 }
