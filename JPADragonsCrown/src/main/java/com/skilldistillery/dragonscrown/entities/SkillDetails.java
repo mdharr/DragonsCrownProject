738232059124e9a442b3b60ae1b16afbe5485333
Michael Harrington <mdharr7@gmail.com>
2024-02-20 21:44:10 +0800

    Stub out remainder of skill details entity.

--- JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/SkillDetails.java
+++ JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/SkillDetails.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.dragonscrown.entities;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -34,6 +36,102 @@ public class SkillDetails {
 	@ManyToOne
 	@JoinColumn(name = "skill_id")
 	private Skill skill;
-	
+
+	public SkillDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SkillDetails(int id, int rank, int requiredSkillPoints, int similarSkillPoints, int requiredPlayerLevel,
+			String effects, Skill skill) {
+		super();
+		this.id = id;
+		this.rank = rank;
+		this.requiredSkillPoints = requiredSkillPoints;
+		this.similarSkillPoints = similarSkillPoints;
+		this.requiredPlayerLevel = requiredPlayerLevel;
+		this.effects = effects;
+		this.skill = skill;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public int getRequiredSkillPoints() {
+		return requiredSkillPoints;
+	}
+
+	public void setRequiredSkillPoints(int requiredSkillPoints) {
+		this.requiredSkillPoints = requiredSkillPoints;
+	}
+
+	public int getSimilarSkillPoints() {
+		return similarSkillPoints;
+	}
+
+	public void setSimilarSkillPoints(int similarSkillPoints) {
+		this.similarSkillPoints = similarSkillPoints;
+	}
+
+	public int getRequiredPlayerLevel() {
+		return requiredPlayerLevel;
+	}
+
+	public void setRequiredPlayerLevel(int requiredPlayerLevel) {
+		this.requiredPlayerLevel = requiredPlayerLevel;
+	}
+
+	public String getEffects() {
+		return effects;
+	}
+
+	public void setEffects(String effects) {
+		this.effects = effects;
+	}
+
+	public Skill getSkill() {
+		return skill;
+	}
+
+	public void setSkill(Skill skill) {
+		this.skill = skill;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SkillDetails other = (SkillDetails) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "SkillDetails [id=" + id + ", rank=" + rank + ", requiredSkillPoints=" + requiredSkillPoints
+				+ ", similarSkillPoints=" + similarSkillPoints + ", requiredPlayerLevel=" + requiredPlayerLevel
+				+ ", effects=" + effects + ", skill=" + skill + "]";
+	}
 	
 }
