65e642c5000730423c5928ad9a748361857f6857
Michael Harrington <mdharr7@gmail.com>
2024-02-21 11:33:57 +0800

    Autowire skill service in skill rest controller and create rest api endpoint for retrieving all skills.

--- DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/controllers/SkillController.java
+++ DragonsCrown/src/main/java/com/skilldistillery/dragonscrown/controllers/SkillController.java
@@ -1,12 +1,35 @@
 package com.skilldistillery.dragonscrown.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.dragonscrown.entities.Skill;
+import com.skilldistillery.dragonscrown.services.SkillService;
 
 @RestController
 @CrossOrigin({"*", "http://localhost"})
 @RequestMapping("api")
 public class SkillController {
 
+	@Autowired
+	private SkillService skillService;
+	
+	@GetMapping("skills")
+	public ResponseEntity<List<Skill>> getAllSkills() {
+		List<Skill> skills = skillService.findAll();
+		
+		if (skills.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		else {
+			return new ResponseEntity<>(skills, HttpStatus.OK);
+		}
+	}
 }
