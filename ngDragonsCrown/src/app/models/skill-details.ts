7f95ce314c28f24bac1163eb4fb6f1e5c05dadd1
Michael Harrington <mdharr7@gmail.com>
2024-02-23 21:08:59 +0800

    Stub out constructor for SkillDetails in frontend model

--- ngDragonsCrown/src/app/models/skill-details.ts
+++ ngDragonsCrown/src/app/models/skill-details.ts
@@ -1,3 +1,5 @@
+import { Skill } from "./skill";
+
 export class SkillDetails {
 
   id: number;
@@ -7,4 +9,22 @@ export class SkillDetails {
   requiredPlayerLevel: number;
   effects: string;
   skill: Skill;
+
+  constructor(
+    id: number = 0,
+    rank: number = 0,
+    requiredSkillPoints: number = 0,
+    similarSkillLevel: number = 0,
+    requiredPlayerLevel: number = 0,
+    effects: string = '',
+    skill: Skill = new Skill()
+  ) {
+    this.id = id;
+    this.rank = rank;
+    this.requiredSkillPoints = requiredSkillPoints;
+    this.similarSkillLevel = similarSkillLevel;
+    this.requiredPlayerLevel = requiredPlayerLevel;
+    this.effects = effects;
+    this.skill = skill;
+  }
 }
