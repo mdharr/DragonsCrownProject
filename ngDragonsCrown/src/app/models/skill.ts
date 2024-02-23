2fc8aa266cb04c829fab0d74af9f8fbd82433870
Michael Harrington <mdharr7@gmail.com>
2024-02-23 20:40:36 +0800

    Stub out constructor for Skill in frontend model

--- ngDragonsCrown/src/app/models/skill.ts
+++ ngDragonsCrown/src/app/models/skill.ts
@@ -4,4 +4,16 @@ export class Skill {
   description: string;
   cardImageUrl: string;
   isCommon: boolean;
+
+  constructor(
+    id: number = 0,
+    description: string = '',
+    cardImageUrl: string = '',
+    isCommon: boolean = false
+  ) {
+    this.id = id;
+    this.description = description;
+    this.cardImageUrl = cardImageUrl;
+    this.isCommon = isCommon;
+  }
 }
