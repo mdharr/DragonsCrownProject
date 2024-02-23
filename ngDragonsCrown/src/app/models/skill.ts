1ee5a183511a301c463d871a23d924f70ca062a7
Michael Harrington <mdharr7@gmail.com>
2024-02-23 20:41:54 +0800

    Add skill details property to skill model

--- ngDragonsCrown/src/app/models/skill.ts
+++ ngDragonsCrown/src/app/models/skill.ts
@@ -1,19 +1,24 @@
+import { SkillDetails } from "./skill-details";
+
 export class Skill {
 
   id: number;
   description: string;
   cardImageUrl: string;
   isCommon: boolean;
+  skillDetails: SkillDetails[];
 
   constructor(
     id: number = 0,
     description: string = '',
     cardImageUrl: string = '',
-    isCommon: boolean = false
+    isCommon: boolean = false,
+    skillDetails: SkillDetails[] = []
   ) {
     this.id = id;
     this.description = description;
     this.cardImageUrl = cardImageUrl;
     this.isCommon = isCommon;
+    this.skillDetails = skillDetails;
   }
 }
