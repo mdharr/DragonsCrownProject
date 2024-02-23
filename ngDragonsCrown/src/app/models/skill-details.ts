8782d34e4ce033fc303642e525be893c5898dbda
Michael Harrington <mdharr7@gmail.com>
2024-02-23 21:06:40 +0800

    Add properties for SkillDetails in frontend model

--- ngDragonsCrown/src/app/models/skill-details.ts
+++ ngDragonsCrown/src/app/models/skill-details.ts
@@ -1,2 +1,10 @@
 export class SkillDetails {
+
+  id: number;
+  rank: number;
+  requiredSkillPoints: number;
+  similarSkillLevel: number;
+  requiredPlayerLevel: number;
+  effects: string;
+  skill: Skill;
 }
