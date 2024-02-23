e7c012d42347377da74293401ea66bf19d01982c
Michael Harrington <mdharr7@gmail.com>
2024-02-23 20:36:42 +0800

    Generate frontend model for Skill Details

---
+++ ngDragonsCrown/src/app/models/skill-details.spec.ts
@@ -0,0 +1,7 @@
+import { SkillDetails } from './skill-details';
+
+describe('SkillDetails', () => {
+  it('should create an instance', () => {
+    expect(new SkillDetails()).toBeTruthy();
+  });
+});
