8ee8d8c7cf25dc5105c673d6248a04b4ed2c41e3
Michael Harrington <mdharr7@gmail.com>
2024-02-27 11:51:41 +0800

    Stub out quest model

--- ngDragonsCrown/src/app/models/quest.ts
+++ ngDragonsCrown/src/app/models/quest.ts
@@ -1,2 +1,25 @@
 export class Quest {
+
+  id: number;
+  name: string;
+  skillPoints: number;
+  description: string;
+  location: string;
+  path: string;
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    skillPoints: number = 0,
+    description: string = '',
+    location: string = '',
+    path: string = '',
+  ) {
+    this.id = id;
+    this.name = name;
+    this.skillPoints = skillPoints;
+    this.description = description;
+    this.location = location;
+    this.path = path;
+  }
 }
