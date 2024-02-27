cafee1288cd924f42230c0264738f32955ea0a77
Michael Harrington <mdharr7@gmail.com>
2024-02-27 11:24:58 +0800

    Stub out player class has quest entity and embeddable id, add to peristence.xml class declarations.

--- JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/PlayerClassHasQuest.java
+++ JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/PlayerClassHasQuest.java
@@ -1,5 +1,90 @@
 package com.skilldistillery.dragonscrown.entities;
 
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "player_class_has_quest")
 public class PlayerClassHasQuest {
 
+	@EmbeddedId
+	private PlayerClassHasQuestId id;
+	
+	@JsonIgnore
+    @ManyToOne
+    @MapsId("playerClassId")
+    @JoinColumn(name = "player_class_id")
+    private PlayerClass playerClass;
+
+    @ManyToOne
+    @MapsId("questId")
+    @JoinColumn(name = "quest_id")
+    private Quest quest;
+
+	public PlayerClassHasQuest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlayerClassHasQuest(PlayerClassHasQuestId id, PlayerClass playerClass, Quest quest) {
+		super();
+		this.id = id;
+		this.playerClass = playerClass;
+		this.quest = quest;
+	}
+
+	public PlayerClassHasQuestId getId() {
+		return id;
+	}
+
+	public void setId(PlayerClassHasQuestId id) {
+		this.id = id;
+	}
+
+	public PlayerClass getPlayerClass() {
+		return playerClass;
+	}
+
+	public void setPlayerClass(PlayerClass playerClass) {
+		this.playerClass = playerClass;
+	}
+
+	public Quest getQuest() {
+		return quest;
+	}
+
+	public void setQuest(Quest quest) {
+		this.quest = quest;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PlayerClassHasQuest other = (PlayerClassHasQuest) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "PlayerClassHasQuest [id=" + id + ", playerClass=" + playerClass + ", quest=" + quest + "]";
+	}
+    
 }
--- JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/PlayerClassHasQuestId.java
+++ JPADragonsCrown/src/main/java/com/skilldistillery/dragonscrown/entities/PlayerClassHasQuestId.java
@@ -1,5 +1,75 @@
 package com.skilldistillery.dragonscrown.entities;
 
-public class PlayerClassHasQuestId {
+import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Table;
+
+@Embeddable
+@Table(name = "player_class_has_quest")
+public class PlayerClassHasQuestId implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "player_class_id")
+	private int playerClassId;
+	
+	@Column(name = "quest_id")
+	private int questId;
+
+	public PlayerClassHasQuestId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlayerClassHasQuestId(int playerClassId, int questId) {
+		super();
+		this.playerClassId = playerClassId;
+		this.questId = questId;
+	}
+
+	public int getPlayerClassId() {
+		return playerClassId;
+	}
+
+	public void setPlayerClassId(int playerClassId) {
+		this.playerClassId = playerClassId;
+	}
+
+	public int getQuestId() {
+		return questId;
+	}
+
+	public void setQuestId(int questId) {
+		this.questId = questId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(playerClassId, questId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PlayerClassHasQuestId other = (PlayerClassHasQuestId) obj;
+		return playerClassId == other.playerClassId && questId == other.questId;
+	}
+
+	@Override
+	public String toString() {
+		return "PlayerClassHasQuestId [playerClassId=" + playerClassId + ", questId=" + questId + "]";
+	}
+	
 }
