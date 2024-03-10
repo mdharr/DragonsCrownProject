20f846a1c8ae6ff6f3570bb43fe67e709b4f16a5
Michael Harrington <mdharr7@gmail.com>
2024-03-10 10:48:12 +0800

    Inject HttpClient and write method to fetch image as data url by hitting server side rest api

--- ngDragonsCrown/src/app/services/image-proxy.service.ts
+++ ngDragonsCrown/src/app/services/image-proxy.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageProxyService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Method to fetch the image through the proxy
+  fetchImageAsDataURL(url: string): Observable<string> {
+    return this.http.get<string>(`/api/proxy/image?url=${encodeURIComponent(url)}`, { responseType: 'text' as 'json' });
+  }
+
 }
