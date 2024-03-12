a7f4bbd8a57ee3803a6b119c22404e2ccc35f5bf
Michael Harrington <mdharr7@gmail.com>
2024-03-12 09:01:08 +0800

    Convert Spell interface to class with constructor.

--- ngDragonsCrown/src/app/models/spell.ts
+++ ngDragonsCrown/src/app/models/spell.ts
@@ -1,9 +1,23 @@
 import { Rune } from "./rune";
 
-export interface Spell {
+export class Spell {
   id: number;
   name: string;
   description: string;
   abbreviation: string;
   runes: Rune[];
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    description: string = '',
+    abbreviation: string = '',
+    runes: Rune[] = []
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.abbreviation = abbreviation;
+    this.runes = runes;
+  }
 }
