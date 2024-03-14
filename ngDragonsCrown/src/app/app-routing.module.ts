62cad000545c602ce1551d487af3dec3d7ed66fd
Michael Harrington <mdharr7@gmail.com>
2024-03-14 16:45:56 +0800

    Add routes for home and build share to app routing module.

--- ngDragonsCrown/src/app/app-routing.module.ts
+++ ngDragonsCrown/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuildShareComponent } from './components/build-share/build-share.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'build/:buildId', component: BuildShareComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
--- ngDragonsCrown/src/app/components/build-share/build-share.component.ts
+++ ngDragonsCrown/src/app/components/build-share/build-share.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class BuildShareComponent {
 
+  // properties
+  build: any;
 }
